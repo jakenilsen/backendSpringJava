304b542af703b02f19399c31cae223a1576b22ea
Jacob <jacobmnilsen@gmail.com>
2021-02-22 20:04:44 +0900

    Added pagination and get all users

--- src/main/java/com/appsdeveloperblogappws/mobileappws/service/UserService.java
+++ src/main/java/com/appsdeveloperblogappws/mobileappws/service/UserService.java
@@ -3,10 +3,13 @@ package com.appsdeveloperblogappws.mobileappws.service;
 import com.appsdeveloperblogappws.mobileappws.shared.dto.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto user);
     UserDto getUser(String email);
     UserDto getUserByUserId(String userId);
     UserDto updateUser(String userId, UserDto user);
     void deleteUser(String userId);
+    List<UserDto> getUsers(int page, int limit);
 }
--- src/main/java/com/appsdeveloperblogappws/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblogappws/mobileappws/service/impl/UserServiceImpl.java
@@ -9,6 +9,9 @@ import com.appsdeveloperblogappws.mobileappws.shared.dto.UserDto;
 import com.appsdeveloperblogappws.mobileappws.ui.model.response.ErrorMessages;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -116,5 +120,27 @@ public class UserServiceImpl implements UserService {
             throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
         }
         userRepository.delete(userEntity);
+    }
+
+    @Override
+    public List<UserDto> getUsers(int page, int limit) {
+        List<UserDto> returnValue = new ArrayList<>();
+
+        if(page > 0) {
+            page -= 1;
+        }
+
+        Pageable pageableRequest = PageRequest.of(page, limit);
+        Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
+
+        List<UserEntity> users = usersPage.getContent();
+
+        for(UserEntity userEntity : users) {
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(userEntity, userDto);
+            returnValue.add(userDto);
+        }
+
+        return returnValue;
     }
 }
